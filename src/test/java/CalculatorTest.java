9c22882b8639ee1ef52c8d987946addaf16c1295
idmacleod <idmacleod@gmail.com>
2020-06-15 15:42:57 +0100

    Initial commit - forgot

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(3, calculator.add(1,2));
+    }
+
+    @Test
+    public void testSubtractPositive(){
+        assertEquals(5, calculator.subtract(9, 4));
+    }
+
+    @Test
+    public void testSubtractNegative(){
+        assertEquals(-3, calculator.subtract(1,4));
+    }
+
+
+
+}
